7ec47c431692ce03b39fc500ad41882ca693deb0
oura-hideyoshi <h.oura@strategit.jp>
2022-10-12 16:17:49 +0900

    ts環境へ移行

---
+++ front-root/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import App from "../components/App";
+
+const Home: NextPage = () => {
+  return (
+    <App>
+      <p>Index Page</p>
+    </App>
+  );
+};
+
+export default Home;
