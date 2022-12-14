86ea84c058e71c2a7806edb3506d1b76b9736550
oura-hideyoshi <tomatommy.bs@gmail.com>
2022-12-14 17:19:06 +0900

    char code translator

--- src/components/template/translate/index.tsx
+++ src/components/template/translate/index.tsx
@@ -1,22 +1,29 @@
 import { Box, Tab, Tabs, Typography } from '@mui/material'
 import Link from 'next/link'
-import { useState } from 'react'
+import { ReactNode, useState } from 'react'
+import CharCode from './char-code'
 import CreateChord from './create-chord'
 import UpperAndLower from './upper-and-lower'
 
 export const TranslatePage = ({ ...props }) => {
   const [index, setIndex] = useState(0)
 
+  const pages: { label: string; content: ReactNode }[] = [
+    { label: 'char code', content: <CharCode /> },
+    { label: 'translate', content: <UpperAndLower /> },
+    { label: 'combine', content: <CreateChord /> },
+  ]
+
   return (
     <Box>
       <Link href="/">back to home</Link>
       <Typography variant={'h2'}>translate letters</Typography>
-      <Tabs value={index} onChange={(_, value) => setIndex(value)}>
-        <Tab label={'translate'} />
-        <Tab label={'combine'} />
+      <Tabs value={index} onChange={(_, value) => setIndex(value)} sx={{ mb: 2 }}>
+        {pages.map((page) => (
+          <Tab key={page.label} label={page.label} />
+        ))}
       </Tabs>
-      {index == 0 && <UpperAndLower />}
-      {index == 1 && <CreateChord />}
+      {pages[index].content}
     </Box>
   )
 }
