c83b92a50901ebd4f64f12947494cff07f579ffe
oura-hideyoshi <h.oura@strategit.jp>
2023-04-19 23:04:06 +0900

    ChordNode 設置

--- src/components/commons/ChordNode.tsx
+++ src/components/commons/ChordNode.tsx
@@ -1,11 +1,15 @@
+import { Chord } from '@tonaljs/tonal'
 import { memo } from 'react'
 import { Handle, NodeProps, Position } from 'reactflow'
 
-const ChordNode = memo(({ ...props }: NodeProps) => {
+const ChordNode = memo(({ ...props }: NodeProps<string>) => {
+  const chord = Chord.get(props.data)
+  const symbol = chord.empty ? '?' : chord.symbol
+
   return (
     <>
       <Handle type="target" position={Position.Left} />
-      <div style={{ padding: 20 }}>a</div>
+      <div style={{ padding: 20 }}>{symbol}</div>
       <Handle type="source" position={Position.Right} />
     </>
   )
--- src/function/getInitState.ts
+++ src/function/getInitState.ts
@@ -5,7 +5,7 @@ function getInitState() {
   const initialNodes: Node[] = [
     { id: '1', type: 'input', position: { x: 0, y: 0 }, data: { label: '1' } },
     { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-    { id: '3', type: 'ChordNode', position: { x: 0, y: 200 }, data: { label: '3' } },
+    { id: '3', type: 'ChordNode', position: { x: 0, y: 200 }, data: 'A' },
   ]
   const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }]
   const nodeTypes: NodeTypes = {
