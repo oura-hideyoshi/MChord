128cb960a6d17b3b1d800c649e5248e895d25313
oura-hideyoshi <h.oura@strategit.jp>
2023-05-05 17:51:06 +0900

    fix: node typeのmemo化を解除

--- src/function/getInitState.ts
+++ src/function/getInitState.ts
@@ -1,21 +1,18 @@
 import { Node } from 'reactflow'
 import ChordNode from '../components/commons/ChordNode'
-import { useMemo } from 'react'
 
-function useInitState() {
+function getInitState() {
   const initialNodes: Node[] = [
     { id: '1', type: 'input', position: { x: 0, y: 0 }, data: { label: '1' } },
     { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
     { id: '3', type: 'ChordNode', position: { x: 0, y: 200 }, data: 'A' },
   ]
   const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }]
-  const nodeTypes = useMemo(() => {
-    return {
-      ChordNode: ChordNode,
-    }
-  }, [])
+  const nodeTypes = {
+    ChordNode: ChordNode,
+  }
 
   return { initialNodes, initialEdges, nodeTypes }
 }
 
-export default useInitState
+export default getInitState
