e61141b1be806c64dcaeb650f9d3c021c5ee0cc0
oura-hideyoshi <h.oura@strategit.jp>
2023-05-06 11:23:21 +0900

    fix: initial nodes

--- src/function/useInitState.ts
+++ src/function/useInitState.ts
@@ -1,12 +1,12 @@
 import { Node } from 'reactflow'
 import { useMemo } from 'react'
 import { nodeTypes as _nodeTypes, nodeTypeNames } from '../const/nodeTypes'
+import { ChordNodeData } from '../type/NodeData'
 
 function useInitState() {
-  const initialNodes: Node[] = [
-    { id: '1', type: 'input', position: { x: 0, y: 0 }, data: { label: '1' } },
-    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-    { id: '3', type: nodeTypeNames.ChordNode, position: { x: 0, y: 200 }, data: 'A' },
+  const initialNodes: Node<ChordNodeData>[] = [
+    { id: '1', type: nodeTypeNames.ChordNode, position: { x: 0, y: 200 }, data: { chordName: 'c', key: 'c' } },
+    { id: '2', type: nodeTypeNames.ChordNode, position: { x: 200, y: 200 }, data: { chordName: 'd', key: 'c' } },
   ]
   const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }]
 
